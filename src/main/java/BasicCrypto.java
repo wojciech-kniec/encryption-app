6d4723f46d23512df8e13ac30b4425a0dad0c1d0
wojciech-kniec <kniecwojciech@gmail.com>
2019-06-28 15:52:46 +0200

    Added Encryption overrided methods

--- src/main/java/BasicCrypto.java
+++ src/main/java/BasicCrypto.java
@@ -1,9 +1,23 @@
 public class BasicCrypto implements Crypto {
+
+
     public byte[] encrypt(byte[] data) {
-        return data;
+        byte[] enc = new byte[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            enc[i] = (byte) ((i % 2 == 0) ? data[i] + 1 : data[i] - 1);
+
+        }
+        return enc;
     }
 
+
     public byte[] decrypt(byte[] data) {
-        return data;
+        byte[] enc = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            enc[i] = (byte) ((i % 2 == 0) ? data[i] - 1 : data[i] + 1);
+
+        }
+        return enc;
     }
 }
